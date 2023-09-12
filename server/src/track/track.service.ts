b8bf2bc4540faa696fa57f6cbc91668c23c4e8a4
NPavlovaaa <n.pavlova.web@gmail.com>
2023-09-12 20:48:33 +0300

    Setup server; create Track and Comment models and schemas; add database connection

---
+++ server/src/track/track.service.ts
@@ -0,0 +1,17 @@
+import {Injectable} from "@nestjs/common";
+
+@Injectable()
+export class TrackService{
+    async create(){
+
+    }
+    async getAll(){
+        return 'All users'
+    }
+    async getOne(){
+
+    }
+    async delete(){
+
+    }
+}
