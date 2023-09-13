d2d214a5c456bc8f91f6d74eeaecb4054d691a82
NPavlovaaa <n.pavlova.web@gmail.com>
2023-09-13 17:17:38 +0300

    Create dto CreateTrackDto, add create and getAll methods

--- server/src/track/track.service.ts
+++ server/src/track/track.service.ts
@@ -1,16 +1,31 @@
 import {Injectable} from "@nestjs/common";
+import {InjectModel} from "@nestjs/mongoose";
+import {Model} from "mongoose";
+import {Track, TrackDocument} from "./schemas/track.schema";
+import {CreateTrackDto} from "./dto/create-track.dto";
+import {Comment, CommentDocument} from "./schemas/comment.schema";
 
 @Injectable()
 export class TrackService{
-    async create(){
+    constructor(@InjectModel(Track.name) private trackModel: Model<TrackDocument>,
+                @InjectModel(Comment.name) private commentModel: Model<CommentDocument>,
+                ) {}
+
+    async create(dto: CreateTrackDto): Promise<Track>{
+        const track = await this.trackModel.create({...dto, listens: 0});
+        return track
+    }
+
+    async getAll(): Promise<Track[]>{
+        const tracks = await this.trackModel.find();
+        return tracks
 
     }
-    async getAll(){
-        return 'All users'
-    }
+
     async getOne(){
 
     }
+
     async delete(){
 
     }
